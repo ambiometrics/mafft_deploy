a053e21bc02f2e9360869816b7162392be557845
Edwin Rodríguez <edwrodrig@gmail.com>
2019-11-25 16:17:31 -0300

    First windows_nt launch

--- 01.prepare.sh
+++ 01.prepare.sh
@@ -6,16 +6,15 @@ rm -rf sources
 mkdir sources && cd sources
 
 #download the linux binary
-wget -O mafft.tgz https://mafft.cbrc.jp/alignment/software/mafft-7.450-linux.tgz
+wget -O mafft.zip https://mafft.cbrc.jp/alignment/software/mafft-7.450-win64-signed.zip
 
 #unzip mafft
-rm -rf mafft
-mkdir mafft
-tar xfvz mafft.tgz -C mafft --strip-components 1
+rm -rf mafft-win
+unzip mafft.zip
 
 #the executable is in a folden names sources/mafft-linux64
 #so move to linux folder to make more standard
 cd ..
 rm -rf release
-cp -rf sources/mafft release
+cp -rf sources/mafft-win release
 
--- release/mafft.bat
+++ release/mafft.bat
@@ -1,8 +1,33 @@
-#! /bin/bash
-# sh -> bash for debian. By J. R. Peterson. 2015/Jun.
+@echo off
+setlocal enabledelayedexpansion
+cls; 1>&2
+chcp 65001 1>&2
+for /f "usebackq tokens=*" %%i IN (`cd`) DO @set current_dir=%%i
+if /i "%current_dir%" == "%systemroot%" (
+  set mafft_working_dir="%~dp0"
+) else (
+  set mafft_working_dir="%current_dir%"
+)
+pushd "%mafft_working_dir%"
 
-pushd "`dirname "$0"`" > /dev/null 2>&1; rootdir="$PWD"; popd > /dev/null 2>&1;
-MAFFT_BINARIES="$rootdir/mafftdir/libexec"; export MAFFT_BINARIES;
+echo; 1>&2
+echo Preparing environment to run MAFFT on Windows. 1>&2
+echo This may take a while, if real-time scanning by anti-virus software is on. 1>&2
 
-"$rootdir/mafftdir/bin/mafft" "$@"
-# input file name can have space
+set ROOTDIR=%~d0%~p0
+set PATH=/usr/bin/:%PATH%
+set MAFFT_BINARIES=/usr/lib/mafft
+set TMPDIR=%TMP%
+set MAFFT_TMPDIR=%TMPDIR%
+
+REM set TMPDIR=%ROOTDIR%/tmp
+REM set MAFFT_TMPDIR=%TMPDIR%
+REM If you do not have write permission for Windows temporary folder
+REM (typically C:\Users\username\AppData\Local\Temp\), then
+REM uncomment (remove REM) the above two lines to use an alternative 
+REM temporary folder.
+
+"%ROOTDIR%\usr\bin\bash" "/usr/bin/mafft" %*
+
+popd
+exit /b
